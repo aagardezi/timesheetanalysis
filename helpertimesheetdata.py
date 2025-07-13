c675593fb048bcc6cccce525d3072f29068d7d02
Ali Gardezi <sgardezi@google.com>
2025-07-13 21:36:08 +0100

    query updated

--- helpertimesheetdata.py
+++ helpertimesheetdata.py
@@ -4,9 +4,9 @@ import helpercode
 PROJECT_ID = helpercode.get_project_id()
 
 def get_timesheet_data():
-    sql = """SELECT * FROM `genaillentsearch.bhtimesheetdemo.TimesheetApprovalHistory` a
-            inner join `genaillentsearch.bhtimesheetdemo.BHTimeUsers` b
-            on a.TimesheetOwnerEmail = b.EmailAddress"""
+    sql = """SELECT a.*, b.TimesheetApprover, b.Employee,b.Division, b.Security, b.Status FROM `genaillentsearch.bhtimesheetdemo.TimesheetApprovalHistory` a
+inner join `genaillentsearch.bhtimesheetdemo.BHTimeUsers` b
+on a.TimesheetOwnerEmail = b.EmailAddress"""
     timesheetdf = pandas_gbq.read_gbq(sql, project_id=PROJECT_ID)
     return timesheetdf.to_markdown()
 

a61295bbd6f65fa4ff4a14849defa8612a8ad2d7
Ali Gardezi <sgardezi@google.com>
2025-07-13 21:01:14 +0100

    instructions improved

--- main.py
+++ main.py
@@ -257,6 +257,8 @@ You will call the function get_timesheet_data to get the data.
 
 The function call will return a markup data set. this is based on the pandas data frame. 
 
+If code is generated use the code_execution tool that is part of the functions to execute it.
+
 You will receive timesheet data as a markup data set from the above function call, where each record represents a distinct timesheet action or status. The fields available for analysis are precisely defined as follows:
 
     TimesheetStartDate (Date): The start date of the reporting period for the timesheet.
